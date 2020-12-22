856a8b6926c4addbda3554f4e0d65d8c2c03d44d
crazylinuxer <kvirikroma@gmail.com>
2020-12-22 15:10:57 +0200

    caching to tmpfs

--- cacher.py
+++ cacher.py
@@ -5,11 +5,12 @@ import sys
 
 
 UPDATE_PERIOD_SECONDS = 600
+CACHE_FOLDER = '/tmp/bt_headset_charge_cache'
 
 
 def get_last_time() -> int:
     try:
-        seconds_cached = open("cache/last_recheck.int", 'rb')
+        seconds_cached = open(os.path.join(CACHE_FOLDER, "last_recheck.int"), 'rb')
     except:
         return 0
 
@@ -25,14 +26,14 @@ def get_last_time() -> int:
 
 
 def set_last_time(new_unix_time: int):
-    seconds_cached = open("cache/last_recheck.int", 'wb')
+    seconds_cached = open(os.path.join(CACHE_FOLDER, "last_recheck.int"), 'wb')
     seconds_cached.write(new_unix_time.to_bytes(4, byteorder='little'))
     seconds_cached.close()
 
 
 def get_last_charge() -> str:
     try:
-        charge_cached = open("cache/last_charge.txt", 'r')
+        charge_cached = open(os.path.join(CACHE_FOLDER, "last_charge.txt"), 'r')
     except:
         return ''
 
@@ -47,19 +48,19 @@ def get_last_charge() -> str:
 
 
 def set_last_charge(charge: str):
-    charge_cached = open("cache/last_charge.txt", 'w')
+    charge_cached = open(os.path.join(CACHE_FOLDER, "last_charge.txt"), 'w')
     charge_cached.write(charge.rstrip('\r\n'))
     charge_cached.close()
 
 
 def main():
-    os.makedirs("cache", exist_ok=True)
+    os.makedirs(CACHE_FOLDER, exist_ok=True)
     if len(sys.argv) < 2:
         return
     unix_time = int(datetime.datetime.utcnow().timestamp())
     last_unix_time = get_last_time()
     charge = get_last_charge()
-    if (len(sys.argv) > 2 and sys.argv[2] == "force-refresh") or (unix_time - last_unix_time) > UPDATE_PERIOD_SECONDS:
+    if (not last_unix_time or not charge) or (len(sys.argv) > 2 and sys.argv[2] == "force-refresh") or (unix_time - last_unix_time) > UPDATE_PERIOD_SECONDS:
         set_last_time(unix_time)
         proc = subprocess.Popen(args=['./get_charge_immediately.sh', sys.argv[1]], stdout=subprocess.PIPE)
         charge = proc.stdout.read().decode()
